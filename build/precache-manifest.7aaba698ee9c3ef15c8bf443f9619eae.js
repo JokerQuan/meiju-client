8ff51080308def66b45dfbd80a42d9ade6ec94df
JokerQuan <jokeshuan@gmail.com>
2019-06-12 01:38:13 +0800

    编译

---
+++ build/precache-manifest.7aaba698ee9c3ef15c8bf443f9619eae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba9ffe4b6ebcdbafd3590a9a2fd0841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb3aeb1d6ff2f10c6311",
+    "url": "/static/css/2.ce3dc737.chunk.css"
+  },
+  {
+    "revision": "5ed90afe2681bb327efa",
+    "url": "/static/css/main.b3a01218.chunk.css"
+  },
+  {
+    "revision": "bb3aeb1d6ff2f10c6311",
+    "url": "/static/js/2.74a3379d.chunk.js"
+  },
+  {
+    "revision": "5ed90afe2681bb327efa",
+    "url": "/static/js/main.f9d52bc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c07e4962f26bffaa85a0d6cd66b598e",
+    "url": "/static/media/1.5c07e496.png"
+  },
+  {
+    "revision": "60200315ff18238536585f1fac1c70aa",
+    "url": "/static/media/2.60200315.png"
+  },
+  {
+    "revision": "1a2fc93acf3743ceef700827f6965b00",
+    "url": "/static/media/3.1a2fc93a.png"
+  },
+  {
+    "revision": "741f9cd6d1ce971cbb9a95b44f0aab42",
+    "url": "/static/media/4.741f9cd6.png"
+  },
+  {
+    "revision": "67b9bc8966055fdf0d625764a5ea3be7",
+    "url": "/static/media/5.67b9bc89.png"
+  },
+  {
+    "revision": "e9a56ee91969e345ad8228df4698b903",
+    "url": "/static/media/6.e9a56ee9.png"
+  },
+  {
+    "revision": "e2d8bc8003fa8e690d3939ed0a7e84ee",
+    "url": "/static/media/breaking_bad.e2d8bc80.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.391b0947425594655259abba0ec49ebc.js"
+  "/precache-manifest.7aaba698ee9c3ef15c8bf443f9619eae.js"
 );
 
 self.addEventListener('message', (event) => {
